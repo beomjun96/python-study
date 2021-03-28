2e234e575d1bae90158c17fb1ef417b2391d1eef
beomjun96 <80663025+beomjun96@users.noreply.github.com>
2021-03-29 01:07:31 +0900

    Delete 1.computer programming directory

--- 1.computer programming/2nameage.py
+++
@@ -1,4 +0,0 @@
-name=input("이름을 입력해주세요:")
-print(name,"님 반갑습니다.", sep="")
-age=int(input("몇 살이십니까?:"))
-print("10년 후면",age+10,"살이 되시는군요!")
--- 1.computer programming/2pi.py
+++
@@ -1,7 +0,0 @@
-pi = 3.141592
-r = int(input("반지름을 입력해 주세요:",))
-volume = 4/3*pi*r**3
-print(volume)
-
-import math
-print(math.pi)
