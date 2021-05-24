60fc851c001aed7903896fc5b9034672ab7e73d1
beomjun96 <80663025+beomjun96@users.noreply.github.com>
2021-05-25 01:14:20 +0900

    Update practice1.py

--- quiz2/practice1.py
+++ quiz2/practice1.py
@@ -1,40 +1,41 @@
-class Machine:
-    def __init__(self, cupCount=1):
+class Machine():
+    def __init__(self, cupCount = 1):
         self.cupCount = cupCount
 
     def showData(self):
-        return
-
-
-class CoinIn(Machine):
-    def __init__(self, cupCount ,coin, change):
-        super().__init__(cupCount)
-        self.coin = coin
-        self.change = change
         if self.cupCount == 0:
             print("can't not divide with 0.")
-            
-
         
-    def culc(self, cupCount):
-        super().cupCount = cupCount
-        cof_price = 200
-        if super().cupCount * cof_price >= self.coin:
-            self.change = super().cupCount*cof_price - self.coin
-            return self.change
-
         else:
-            return "요금이 부족합니다.(커피 한잔에 200원"
+            if culcChangeCulc >= 0:
+                print("커피 {}잔과 잔돈 {}원".format(self.cupCount, culcChangeCulc))
+
+            else:
+                print("요금이 부족합니다(커피 한잔에 200원)")
 
 
-word = 0
-while word!="n" or word!="N":
+class CoinIn():
+    def __init__(self, coin):
+        self.coin = coin
+        self.change = None  # 돈을 넣을 때 거스름돈을 입력하지 않고 계산 후 출력만 받음
+
+    def culc(self, cupCount):
+        self.cupCount = cupCount
+        self.change = self.coin - self.cupCount * 200
+        return self.change
+
+
+
+while True:
     coin = int(input("코인을 입력하세요:"))
     cup = int(input("몇 잔을 원하세요:"))
 
-    input_data = CoinIn()
-    print(input_data.culc())
+    culcChange = CoinIn(coin)
+    culcChangeCulc = culcChange.culc(cup)
 
+    machineShow = Machine(cup)
+    machineShow.showData()
 
     word = input("다시 입력하시겠습니까? (Stop: 'n' or 'N')")
-
+    if word == "n" or word =="N":
+        break
